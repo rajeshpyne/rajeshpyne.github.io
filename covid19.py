a0cb694390b213e5fbc8feed1c8ab4b69707de73
Rajesh Pyne <rajesh.pyne@gmail.com>
2020-03-24 00:59:18 +0530

    Updated script

--- covid19.py
+++ covid19.py
@@ -22,6 +22,8 @@ india_stat = (results["latest_stat_by_country"][0])
 df = (json_normalize(india_stat))
 #print(df.info())
 #df.to_csv('india_covid19.csv',index=False)
+df = df.drop(columns=["id"])
+df = df.rename(columns={"country_name": "Country", "total_cases": "Total Cases","new_cases":"New Cases","active_cases":"Active Cases","total_deaths":"Total Deaths","new_deaths":"New Deaths","total_recovered":"Total Recovered","serious_critical":"Serious Critical","region":"Region","total_cases_per1m":"Total Cases/Million","record_date":"Last Updated(UTC)"})
 covid19_html = df.to_html(index=False)
 
 #write html to file
